f961a7e3bae67673b2e86e6e6a80dff7ec90fb29
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-07-08 19:16:27 +0400

    develop: legacy version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,10 @@
     console.log(reason)
   })
   abortControllerFast.abort('Abort controller is working')
+
+  let value = 0
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => { value++ }}>Value: {value}</button>
--- static/lib/polyfills/shimport/polyfill.js
+++ static/lib/polyfills/shimport/polyfill.js
@@ -1,18 +1,19 @@
-try {
-  new Function("if(0)import('')")();
-} catch (e) {
+(function() {
+// try {
+//   new Function("if(0)import('')")();
+// } catch (e) {
   var scripts = document.querySelectorAll('script[type=module]');
-  for (var i = 0; i < scripts.length; i++){
+  for (var i = 0; i < scripts.length; i++) {
     var script = scripts[i];
     var newScript = document.createElement("script");
     var attributes = script.attributes;
-    for (var j = 0; j < attributes.length; j++){
+    for (var j = 0; j < attributes.length; j++) {
       var attribute = attributes[j];
-      if (attribute.name !== 'type') {
+      if (attribute.name !== 'type' && attribute.name !== 'src') {
         newScript.setAttribute(attribute.name, attribute.value);
       }
     }
-    var blob = new Blob([script.innerText], {
+    var blob = new Blob(['debugger;' + script.innerText], {
       type: 'application/javascript'
     });
     newScript.setAttribute("data-main", URL.createObjectURL(blob));
@@ -21,4 +22,5 @@ try {
     script.parentNode.insertBefore(newScript, script);
     script.remove();
   }
-}
+// }
+})();
